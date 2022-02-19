c0e4f0095cd2aaf49324780f8cf068570b74d73a
Lukas <lukas.ikvm1@gmail.com>
2022-02-19 12:26:28 +0100

    Add StudentRest basic CRUD methods

--- src/main/java/se/iths/rest/StudentRest.java
+++ src/main/java/se/iths/rest/StudentRest.java
@@ -1,7 +1,55 @@
 package se.iths.rest;
 
+import se.iths.entity.Student;
+import se.iths.service.StudentService;
 
+import javax.ejb.PostActivate;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("students")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class StudentRest {
 
+    @Inject
+    StudentService studentService;
 
+    @Path("")
+    @POST
+    public Response createStudent(Student student){
+        studentService.createStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("")
+    @PUT
+    public Response updateStudent(Student student){
+        studentService.updateStudent(student);
+        return Response.ok(student).build();
+    }
+
+    @Path("{id}")
+    @GET
+    public Response getStudent(@PathParam("id") Long id){
+        Student foundStudent = studentService.findStudentById(id);
+        return Response.ok(foundStudent).build();
+    }
+
+    @Path("")
+    @GET
+    public Response getAllStudents(){
+        List<Student> foundStudents = studentService.getAllStudents();
+        return Response.ok(foundStudents).build();
+    }
+
+    @Path("{id}")
+    @DELETE
+    public Response deleteStudent(@PathParam("id") Long id ){
+        studentService.deleteStudent(id);
+        return Response.ok().build();
+    }
 }
